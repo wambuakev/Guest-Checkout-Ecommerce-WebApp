e4415bb3c413ecef68783ab1553aeb0930589f43
KELVIN WAMBUA NZIOKI <kelvinwambua069@gmail.com>
2023-02-14 12:07:39 +0300

    Render templates for Login/Register

--- store/urls.py
+++ store/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     #Space for the base url
+    path('register/', views.registerPage, name="register"),
+    path('login/', views.loginPage, name="login"),
+    
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
     path('checkout/', views.checkout, name="checkout"),
